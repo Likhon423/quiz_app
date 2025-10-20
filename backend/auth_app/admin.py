8655f7a203a48a5bebf1b108b28e89351e228022
rakibul haque <likhon423@gmail.com>
2025-10-20 12:55:58 +0600

    user registration completed

---
+++ backend/auth_app/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_superuser')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role Information', {'fields': ('role',)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Role Information', {'fields': ('role',)}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
