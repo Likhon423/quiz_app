3f415bb4f44f61915979fd09489b5499a66c0d80
rakibul haque <likhon423@gmail.com>
2025-10-29 09:41:14 +0600

    multiple question sava, update, delete using javascript api

--- backend/quiz/api.py
+++ backend/quiz/api.py
@@ -7,8 +7,6 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .models import Quiz, Question, Option
-from .forms import QuestionForm
-
 
 
 def serialize_question(question):
@@ -21,32 +19,24 @@ def serialize_question(question):
         'blank_word': question.correct_answer if question.is_fill_in_blank else '',
     }
 
-@login_required
-def get_questions(request, quiz_id):
-    quiz = get_object_or_404(Quiz, id=quiz_id, tutor=request.user)
-    questions = quiz.questions.all()
-    data = [serialize_question(q) for q in questions]
-    return JsonResponse({'question': data})
-
-
-@csrf_exempt
-@login_required
-@require_http_methods(["POST"])
-def create_question(request, quiz_id):
-    quiz = get_object_or_404(Quiz, id=quiz_id, tutor=request.user)
-    data = json.loads(request.body.decode('utf-8'))
-
-    question_text = data.get('question_text').strip()
-    question_type = data.get('question_type').strip()
+def save_or_update_question(quiz, data, questionObj=None):
+    question_text = data.get("question_text").strip()
+    question_type = data.get("question_type").strip()
 
     if not question_text or not question_type:
-        return JsonResponse({'success': False, 'error': 'Missing fields.'}, status=400)
+        return JsonResponse({"success": False, "error": "Invalid data"})
     
-    question = Question.objects.create(
-        quiz = quiz,
-        question_text = question_text,
-        question_type = question_type,
-    )
+    if questionObj:
+        question = questionObj
+    else:
+        question = Question.objects.create(
+            quiz = quiz,
+            question_text = question_text,
+            question_type = question_type,
+        )
+    
+    question.question_text = question_text
+    question.question_type = question_type
 
     if question.is_mcq:
         options = data.get('options', [])
@@ -76,4 +66,46 @@ def create_question(request, quiz_id):
             question.correct_answer = blank_word
             question.save()
 
-    return JsonResponse({'success': True, 'question': serialize_question(question)})
+    return {'success': True, 'question': serialize_question(question)}
+
+
+
+@login_required
+def get_questions(request, quiz_id):
+    quiz = get_object_or_404(Quiz, id=quiz_id, tutor=request.user)
+    questions = quiz.questions.all()
+    data = [serialize_question(q) for q in questions]
+    return JsonResponse({'questions': data})
+
+
+@login_required
+@csrf_exempt
+@require_http_methods(["POST"])
+def save_question(request, quiz_id):
+    quiz = get_object_or_404(Quiz, id=quiz_id, tutor=request.user)
+    data = json.loads(request.body.decode('utf-8'))
+
+    result = save_or_update_question(quiz, data)
+    return JsonResponse(result, status=200 if result['success'] else 400)
+
+
+@login_required
+@csrf_exempt
+@require_http_methods(["PUT"])
+def update_question(request, quiz_id, question_id):
+    quiz = get_object_or_404(Quiz, id=quiz_id, tutor=request.user)
+    question = get_object_or_404(Question, id=question_id, quiz=quiz)
+
+    data = json.loads(request.body)
+    result = save_or_update_question(quiz, data, question)
+    return JsonResponse(result, status=200 if result['success'] else 400)
+
+
+@login_required
+@csrf_exempt
+@require_http_methods(["DELETE"])
+def delete_question(request, quiz_id, question_id):
+    quiz = get_object_or_404(Quiz, id=quiz_id, tutor=request.user)
+    question = get_object_or_404(Question, id=question_id, quiz=quiz)
+    question.delete()
+    return JsonResponse({"success": True})
--- backend/quiz/urls.py
+++ backend/quiz/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import createQuizView, addQuestionView
+from . import api
 
 urlpatterns = [
     path('create/', createQuizView, name='create_quiz'),
     path('<int:quiz_id>/addQuestion/', addQuestionView, name="add_question"),
+    path('api/<int:quiz_id>/questions/', api.get_questions, name="get_questions"),
+    path('api/<int:quiz_id>/questions/save/', api.save_question, name="save_question"),
+    path('api/<int:quiz_id>/questions/update/<int:question_id>/', api.update_question, name="update_question"),
+    path('api/<int:quiz_id>/questions/delete/<int:question_id>/', api.delete_question, name="delete_question"),
 ]
